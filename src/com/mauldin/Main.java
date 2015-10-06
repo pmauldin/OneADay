25c17ffc29e1513599daea0e947d1bc28b77859c
Peter Mauldin <petermauldin@utexas.edu>
2015-10-06 00:57:15 -0500

    Created project

---
+++ src/com/mauldin/Main.java
@@ -0,0 +1,10 @@
+package com.mauldin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Subscriber subscriber = new Subscriber("Peter", "Mauldin", "petermauldin@utexas.edu");
+
+        subscriber.printSubscriberInfo();
+    }
+}
---
+++ src/com/mauldin/Subscriber.java
@@ -0,0 +1,15 @@
+package com.mauldin;
+
+public class Subscriber {
+    String firstName, lastName, email;
+
+    public Subscriber(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void printSubscriberInfo() {
+        System.out.printf("Subscriber Information\nName: %s %s\nEmail Address: %s\n", firstName, lastName, email);
+    }
+}
