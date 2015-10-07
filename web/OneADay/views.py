55a47173521460bde597afa69e6500502965f202
Peter Mauldin <petermauldin1@gmail.com>
2015-10-07 02:17:27 -0500

    Got interests from DB

--- web/OneADay/views.py
+++ web/OneADay/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Subscriber
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'OneADay/index.html', {'subscribers': Subscriber.objects.all()})
+
+def select(request):
+	person = Subscriber.objects.get(firstname=request.POST['person'])
+	interests = person.keywords.all()
+
+	return render(request, 'OneADay/interests.html', {'interests': interests})
